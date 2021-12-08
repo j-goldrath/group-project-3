bd51f8040d58e14555676fb83594315997583995
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-12-08 11:34:01 -0600

    added pages and few components, pulling

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import FundraiserList from "../components/FundraiserList";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <FundraiserList />
+        </div>
+    );
+};
+
+export default Home;
