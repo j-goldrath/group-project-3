df3d3e549357fed419230213b4bdd910df281fca
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-12-14 08:07:38 -0600

    Heroku troubleshooting

--- client/src/pages/FundraiserDetails.js
+++
@@ -1,6 +0,0 @@
-// import { useEffect, useState } from 'react';
-// import { Link, useParams } from 'react-router-dom';
-// import { useQuery } from '@apollo/client';
-
-// import Fundraiser from '../components/Fundraiser';
-// import DonateButton from '../components/DonateButton';
--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./user');
 const Fundraiser = require('./Fundraiser');
 const Donation = require('./Donation');
 
